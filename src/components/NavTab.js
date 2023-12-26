b7917630258daa414b4ab94f8122ebdc938ba0c5
Georgy Akopov <akopovzh@yandex.ru>
2023-12-26 16:10:35 +0300

    last update level-2

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const NavTab = () => {
   return (
     <ul className="button-information">
-      <li href="#info" className="button">
-        О проекте
+      <li className="button">
+        <a href="#info">О проекте</a>
       </li>
-      <li href="#techs" className="button">
-        Технологии
+      <li className="button">
+        <a href="#techs">Технологии</a>
       </li>
-      <li href="#portfolio" className="button">
-        Студент
+      <li className="button">
+        <a href="#portfolio">Студент</a>
       </li>
     </ul>
   );
